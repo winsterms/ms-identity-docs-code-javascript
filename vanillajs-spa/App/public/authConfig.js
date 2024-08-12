980c6a00408337fa989590b339b8675eb3f517d7
winster <117750774+winsterms@users.noreply.github.com>
2024-08-12 10:53:11 +0100

    Update authConfig.js

--- vanillajs-spa/App/public/authConfig.js
+++ vanillajs-spa/App/public/authConfig.js
@@ -6,9 +6,9 @@
 const msalConfig = {
     auth: {
 
-        clientId: "Enter_the_Application_Id_Here", // This is the ONLY mandatory field that you need to supply
+        clientId: "9882dad2-93e9-4f36-bab7-44f3760c4569", // This is the ONLY mandatory field that you need to supply
         // WORKFORCE TENANT
-        authority: "https://login.microsoftonline.com/Enter_the_Tenant_Info_Here", //  Replace the placeholder with your tenant info
+        authority: "https://login.microsoftonline.com/30b548af-63d5-48f3-bd8f-c25380174f2e", //  Replace the placeholder with your tenant info
         // EXTERNAL TENANT
         // authority: "https://Enter_the_Tenant_Subdomain_Here.ciamlogin.com/", // Replace the placeholder with your tenant subdomain
         redirectUri: '/', // You must register this URI on App Registration. Defaults to window.location.href e.g. http://localhost:3000/
